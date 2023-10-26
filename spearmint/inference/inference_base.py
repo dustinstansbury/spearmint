c98ef2be636da094c6ca0c7b1d48da72fa92fa1e
Dustin Stansbury <dustin.stansbury@gmail.com>
2023-10-26 10:44:10 -0700

    Refactor inference_base; add tests

--- spearmint/inference/inference_base.py
+++ spearmint/inference/inference_base.py
@@ -1,107 +1,300 @@
-import numpy as np
-from scipy.stats import norm
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+
+from holoviews import Element
+
+from spearmint.typing import FilePath, List
+from spearmint.mixin import DataframeableMixin
+from spearmint.stats import Samples, SamplesComparisonTable, DEFAULT_ALPHA
+from spearmint.utils import process_warnings
 
 
-class InferenceProcedure(object):
+class InferenceResultsDisplayError(Exception):
+    """Raised when there is an error displaying inference procedure results"""
+
+    pass
+
+
+class InferenceResultsVisualizationError(Exception):
+    """Raised when there is an error visualizaing inference procedure results"""
+
+    pass
+
+
+class InferenceResultsMissingError(Exception):
+    """Raised when there are no results associated with an inference procedure."""
+
+    pass
+
+
+class InferenceResults(DataframeableMixin):
     """
-    Base class for all inference procedures. Must implement the
-    following methods:
-      - `run()`
-      - `make_results()`
+    Base class for displaying and saving statistical inference results
+
+    Parameters
+    ----------
+    control : Samples
+        the control samples
+    variation : Samples
+        the variation samples
+    metric_name : str
+        The name of the metric that is being compared
+    delta: float
+        the absolute difference between the variation and control sample means
+    delta_relative: float
+        the percent difference between the variation and control sample means
+    hypothesis : str
+        Human-readable message for interpreting the experiment
+    alpha : float in (0, 1)
+        The "significance" level, or type I error rate for the experiment
+    accept_hypothesis : boolean
+        Whether or not to accept the alternative hypothesis, based on `p_value`
+        and `alpha` and any correction method.
+    warnings : list[str]
+        A list of any warning messages accumultaed during the test
+    aux : dict
+        Auxillary variables used for displaying or visualizing specific types
+        of tests.
     """
 
-    def __init__(self, method=None, stat_dist=norm, *args, **kwargs):
-        self.method = method
-        self.stat_dist = stat_dist
-
-    def run(
-        self, control_samples, variation_samples, alpha=None, inference_kwargs=None
+    def __init__(
+        self,
+        control: Samples,
+        variation: Samples,
+        metric_name: str,
+        delta: float,
+        delta_relative: float,
+        effect_size: float,
+        hypothesis: str,
+        alpha: float,
+        accept_hypothesis: bool,
+        model_name: str = None,
+        comparision_type: str = None,
+        warnings: List[str] = [],
+        aux: dict = {},
     ):
-        """
-        Run inference procedure and return associated results
-        """
-        raise NotImplementedError("Implement me")
-
-    @property
-    def results(self):
-        if not hasattr(self, "_results"):
-            self._results = self.make_results()
-        return self._results
-
-    def make_results(self):
-        """
-        Format and return a HypothesisTestResults object
-        """
-        raise NotImplementedError("Implement me")
-
-
-class FrequentistProcedure(InferenceProcedure):
-    def __init__(self, hypothesis="larger", *args, **kwargs):
-        super(FrequentistProcedure, self).__init__(*args, **kwargs)
+        self.control = control
+        self.variation = variation
+        self.metric_name = metric_name
+        self.delta = delta
+        self.delta_relative = delta_relative
+        self.effect_size = effect_size
         self.hypothesis = hypothesis
+        self.alpha = alpha
+        self.accept_hypothesis = accept_hypothesis
+        self.model_name = model_name
+        self.comparision_type = comparision_type
+        self.warnings = process_warnings(warnings)
+        self.aux = aux
 
-    def run(
-        self, control_samples, variation_samples, alpha=None, inference_kwargs=None
-    ):
-        raise NotImplementedError("Implement me")
+        # These properties should be updated by the running the inference
+        # self.segmentation = None
 
-    def make_results(self):
-        raise NotImplementedError("Implement me")
+    def display(self) -> None:
+        """Display the inference procedure results to the console"""
+        try:
+            self.summary
+        except Exception as e:
+            raise InferenceResultsDisplayError(e)
 
-    @property
-    def control_name(self):
-        return self.comparison.d2.name
+    def visualize(self, outfile: FilePath = None, *args, **kwargs) -> Element:
+        """Visualize the inference procedure results
 
-    @property
-    def variation_name(self):
-        return self.comparison.d1.name
+        Parameters
+        ----------
+        outfile : FilePath, optional
+            If provided, use to save the output visualization, by default None
 
-    @property
-    def hypothesis_sm(self):
+        Returns
+        -------
+        Element : holoviews.Element
+            A constructed visualization object
+
+        Raises
+        ------
+        InferenceResultsVisualizationError
+            If the `_render_visualization` is not implemented. Note: we do not
+            require `_render_visualization` via abstractmethod, leaving the
+            option for std-out-only reporting
         """
-        Statsmodels-compatible hypothesis
-        """
-        return self.hypothesis if self.hypothesis != "unequal" else "two-sided"
+        try:
+            return self._render_visualization(outfile, *args, **kwargs)
 
-    def accept_hypothesis(self, stat_value):
+        except Exception as e:
+            raise InferenceResultsVisualizationError(e)
+
+    def _render_visualization(self):
+        raise NotImplemented(
+            "No implementation of `_render_visualization` for ",
+            f"class {self.__name__}, cannot execute `.visualize`",
+        )
+
+    def _render_stats_table(self):
+        raise NotImplemented(
+            "No implementation of `_render_stats_table` for ",
+            f"class {self.__name__}, cannot execute `.summary`",
+        )
+
+    def to_csv(self, outfile: FilePath, delimiter=","):
         """
-        Accept the null hypothesis based on the calculated statistic and statitic
-        distribution.
+        Export result to delimiter-separated value file
         """
-        if self.hypothesis == "larger":
-            return stat_value > self.stat_dist.ppf(1 - self.alpha)
-        elif self.hypothesis == "smaller":
-            return stat_value < self.stat_dist.ppf(self.alpha)
-        elif self.hypothesis == "unequal":
-            return abs(stat_value) > self.stat_dist.ppf(1 - self.alpha / 2.0)
-        else:
-            raise ValueError("Unknown hypothesis: {!r}".format(self.hypothesis))
+        results_df = self.to_dataframe()
+        results_df.to_csv(outfile, sep=delimiter, encoding="utf8", index=False)
 
     @property
     def hypothesis_text(self):
-        control_name = self.control_name if self.control_name else "control"
-        variation_name = self.variation_name if self.variation_name else "variation"
-
         if self.hypothesis == "larger":
-            return "{} is larger".format(variation_name)
+            return f"{self.variation.name} is larger"
         elif self.hypothesis == "smaller":
-            return "{} is smaller".format(variation_name)
+            return f"{self.variation.name} is smaller"
         elif self.hypothesis == "unequal":
-            return "{} != {}".format(variation_name, control_name)
+            return f"{self.variation.name} != {self.control.name}"
         else:
-            raise ValueError("Unknown hypothesis: {!r}".format(self.hypothesis))
+            raise ValueError("Unknown hypothesis: {self.hypothesis}")
 
     @property
-    def ci_percents(self):
-        if self.hypothesis == "larger":
-            return (self.alpha, np.inf)
-        elif self.hypothesis == "smaller":
-            return (-np.inf, 1 - self.alpha)
-        elif self.hypothesis == "unequal":
-            return ((self.alpha / 2.0), 1 - (self.alpha / 2.0))
-        else:
-            raise ValueError("Unknown hypothesis: {!r}".format(self.hypothesis))
+    def samples_comparison(self):
+        if not hasattr(self, "_samples_comparison_table"):
+            self._samples_comparison_table = SamplesComparisonTable(
+                self.control, self.variation
+            )
+        self._samples_comparison_table.print()
 
     @property
-    def test_statistic(self):
-        return self.comparison.test_statistic
+    def stats(self):
+        if not hasattr(self, "_stats_table"):
+            self._stats_table = self._render_stats_table()
+        self._stats_table.print()
+
+    @property
+    def summary(self):
+        self.samples_comparison
+        self.stats
+
+    def __repr__(self) -> None:
+        self.summary
+
+    @property
+    def _base_properties(self):
+        """
+        Properties that belong to all inference procedure results
+        """
+        return OrderedDict(
+            [
+                ("control_name", self.control.name),
+                ("control_nobs", self.control.nobs),
+                ("control_mean", self.control.mean),
+                ("control_ci", self.control.confidence_interval(self.alpha)),
+                ("control_var", self.control.var),
+                ("variation_name", self.variation.name),
+                ("variation_nobs", self.variation.nobs),
+                ("variation_mean", self.variation.mean),
+                ("variation_ci", self.variation.confidence_interval(self.alpha)),
+                ("variation_var", self.variation.var),
+                ("metric", self.metric_name),
+                ("delta", self.delta),
+                ("delta_relative", 100 * self.delta_relative),
+                ("effect_size", self.effect_size),
+                ("alpha", self.alpha),
+                ("hypothesis", self.hypothesis),
+                ("accept_hypothesis", self.accept_hypothesis),
+                ("model_name", self.model_name),
+                # ("segmentation", self.segmentation),
+                ("warnings", self.warnings),
+            ]
+        )
+
+    @property
+    def _specific_properties(self) -> OrderedDict:
+        """
+        Properties for specific inference procedures
+        """
+        return OrderedDict()
+
+    def to_dict(self) -> OrderedDict:
+        _dict = self._base_properties
+        _dict.update(self._specific_properties)
+        return _dict
+
+    @property
+    def dict(self) -> OrderedDict:
+        return self.to_dict()
+
+
+class InferenceProcedure(ABC):
+    """
+    Base class for all inference procedures. Must implement the following methods:
+
+        -   `_run_inference()` : runs the inference procedure, updating any internal
+            state needed for displaying and/or visualizing results
+        -   `_make_results()` : generate `InferenceResults` based on the outcome
+            of `_run_inference()`
+    """
+
+    def __init__(
+        self,
+        inference_method: str,
+        hypothesis: str = "larger",
+        alpha: float = DEFAULT_ALPHA,
+    ):
+        self.inference_method = inference_method
+        self.hypothesis = hypothesis
+        self.alpha = alpha
+        self._results = None
+
+    @abstractmethod
+    def _run_inference(
+        self, control_samples: Samples, variation_samples: Samples, **inference_kwargs
+    ) -> None:
+        raise NotImplementedError("Implement me")
+
+    @abstractmethod
+    def _make_results(self) -> InferenceResults:
+        """
+        Generate and return a `InferenceResults` object
+        """
+        raise NotImplementedError("Implement me")
+
+    def run(
+        self,
+        control_samples: Samples,
+        variation_samples: Samples,
+        **inference_kwargs,
+    ) -> InferenceResults:
+        """
+        Run inference procedure on control / variation samples, and report results
+
+        Parameters
+        ----------
+        control : Samples
+            the control samples
+        variation : Samples
+            the variation samples
+        **inference_kwargs: kwargs
+            Any inference-specific arguments to include in the call signature.
+
+        Returns
+        -------
+        results : InferenceResults
+            The results of running the inference procedure
+
+        """
+        self._run_inference(control_samples, variation_samples, **inference_kwargs)
+        self._results = self._make_results()
+        return self.results
+
+    @property
+    def results(self) -> InferenceResults:
+        """
+        Raises
+        ------
+        InferenceResultsMissingError
+            if the `run` method hasn't been executed
+        """
+        if self._results is None:
+            raise InferenceResultsMissingError(
+                "You likely need to execute the `run` method.",
+                " Or check the implementation of `_make_results`",
+            )
+        return self._results
---
+++ test/test_inference.py
@@ -0,0 +1,96 @@
+from collections import OrderedDict
+
+from spearmint.inference import InferenceResults, InferenceProcedure
+from spearmint.typing import DataFrame
+
+
+class ExtendedInferenceResults(InferenceResults):
+    def __init__(self, alpha=0.05, *args, **kwargs):
+        super().__init__(
+            metric_name="test",
+            delta=0.0,
+            delta_relative=0.0,
+            effect_size=0.0,
+            hypothesis="larger",
+            alpha=alpha,
+            accept_hypothesis=False,
+            model_name="test_model",
+            warnings=["a warning"],
+            *args,
+            **kwargs
+        )
+
+    @property
+    def _specific_properties(self) -> OrderedDict:
+        return {"specific_property": "blah"}
+
+    def _render_stats_table(self):
+        class DummyStatsTable:
+            """placeholder stats table with print method"""
+
+            def print(self):
+                pass
+
+        return DummyStatsTable()
+
+
+class ExtendedInferenceProcedure(InferenceProcedure):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.inference_run = False
+
+    def _run_inference(self, control_samples, variation_samples) -> None:
+        self.inference_run = True
+        self.control_samples = control_samples
+        self.variation_samples = variation_samples
+
+    def _make_results(self) -> InferenceResults:
+        return ExtendedInferenceResults(
+            control=self.control_samples,
+            variation=self.variation_samples,
+            alpha=self.alpha,
+        )
+
+
+def validate_results(results, alpha: float = 0.05):
+    assert results.dict["control_name"] == results.dict["variation_name"]
+    assert results.dict["control_nobs"] == results.dict["variation_nobs"]
+    assert results.dict["control_mean"] == results.dict["variation_mean"]
+    assert results.dict["control_ci"] == results.dict["variation_ci"]
+    assert results.dict["control_var"] == results.dict["variation_var"]
+    assert results.dict["metric"] == "test"
+    assert results.dict["delta"] == 0
+    assert results.dict["delta_relative"] == 0
+    assert results.dict["effect_size"] == 0
+    assert results.dict["alpha"] == alpha
+    assert results.dict["hypothesis"] == "larger"
+    assert results.dict["accept_hypothesis"] == False
+    assert results.dict["model_name"] == "test_model"
+    assert results.dict["warnings"] == "a warning"
+    assert results.dict["specific_property"] == "blah"
+
+
+def test_inference_results(test_samples):
+    results = ExtendedInferenceResults(control=test_samples, variation=test_samples)
+    validate_results(results)
+    results.summary
+
+    # Check datarame export support
+    results_df = results.to_dataframe()
+    assert isinstance(results_df, DataFrame)
+    # ExtendedInferenceResults has additional `specific_property` field in its
+    # `_specific_property` dictionary
+    assert len(results_df.columns) == len(results._base_properties) + 1
+
+
+def test_inference_procedure(test_samples):
+    inference_procedure = ExtendedInferenceProcedure(inference_method="test", alpha=0.1)
+    inference_results = inference_procedure.run(
+        control_samples=test_samples, variation_samples=test_samples
+    )
+    assert inference_procedure.inference_run
+    assert inference_procedure.alpha == 0.1
+    assert inference_results == inference_procedure.results
+    validate_results(inference_procedure.results, inference_procedure.alpha)
+
+    inference_procedure.results.summary
