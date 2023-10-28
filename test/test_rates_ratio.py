35842e6322b5c551b5a403b05a37b94030e06b34
Dustin Stansbury <dustinstansbury@gmail.com>
2023-10-28 11:12:17 -0700

    Refactor test_rates_ratio

--- test/test_rates_ratio.py
+++ test/test_rates_ratio.py
@@ -1,47 +1,59 @@
 import pytest
-from abra import Experiment, HypothesisTest
+
+from spearmint import Experiment, HypothesisTest
+from spearmint.utils import generate_fake_observations
 
 
-def test_rates_ratio_larger(counts_data):
-    exp = Experiment(data=counts_data)
+@pytest.fixture()
+def rates_data():
+    return generate_fake_observations(
+        distribution="poisson", n_treatments=6, n_observations=6 * 50, random_seed=123
+    )
+
+
+def test_rates_ratio_larger(rates_data):
+    exp = Experiment(data=rates_data)
     ab_test = HypothesisTest(
-        inference_method='rates_ratio',
-        metric='metric',
-        hypothesis='larger',
-        control='A', variation='C'
+        inference_method="rates_ratio",
+        metric="metric",
+        hypothesis="larger",
+        control="A",
+        variation="C",
     )
     ab_results = exp.run_test(ab_test)
     assert ab_results.accept_hypothesis
 
 
-def test_rates_ratio_smaller(counts_data):
-    exp = Experiment(data=counts_data)
+def test_rates_ratio_smaller(rates_data):
+    exp = Experiment(data=rates_data)
     ab_test = HypothesisTest(
-        inference_method='rates_ratio',
-        metric='metric',
-        hypothesis='smaller',
-        control='A', variation='C'
+        inference_method="rates_ratio",
+        metric="metric",
+        hypothesis="smaller",
+        control="A",
+        variation="C",
     )
     ab_results = exp.run_test(ab_test)
     assert not ab_results.accept_hypothesis
 
-def test_rates_ratio_unequal(counts_data):
-    exp = Experiment(data=counts_data)
+
+def test_rates_ratio_unequal(rates_data):
+    exp = Experiment(data=rates_data)
     ab_test = HypothesisTest(
-        inference_method='rates_ratio',
-        metric='metric',
-        hypothesis='unequal',
-        control='A', variation='C'
+        inference_method="rates_ratio",
+        metric="metric",
+        hypothesis="unequal",
+        control="A",
+        variation="C",
     )
     ab_results = exp.run_test(ab_test)
     assert ab_results.accept_hypothesis
 
-def test_rates_ratio_aa(counts_data):
-    exp = Experiment(data=counts_data)
+
+def test_rates_ratio_aa(rates_data):
+    exp = Experiment(data=rates_data)
     aa_test = HypothesisTest(
-        inference_method='rates_ratio',
-        metric='metric',
-        control='A', variation='A'
+        inference_method="rates_ratio", metric="metric", control="A", variation="A"
     )
     aa_results = exp.run_test(aa_test)
     assert not aa_results.accept_hypothesis
