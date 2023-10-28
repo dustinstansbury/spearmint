f9dde4c9a2508b4792a8f385f3964646fad07213
Dustin Stansbury <dustinstansbury@gmail.com>
2023-10-28 11:06:01 -0700

    Update test_hypothesis_test

--- test/test_hypothesis_test.py
+++ test/test_hypothesis_test.py
@@ -1,10 +1,19 @@
+import pytest
 import numpy as np
+from spearmint.utils import generate_fake_observations
 from spearmint.experiment import Experiment
-from spearmint.hypothesis_test import HypothesisTest, HypothesisTestSuite, CustomMetric
+from spearmint.hypothesis_test import HypothesisTest, HypothesisTestGroup, CustomMetric
 
 
-def test_hypothesis_test_suite(proportions_data_large):
-    exp = Experiment(proportions_data_large, name="test")
+@pytest.fixture()
+def test_data():
+    return generate_fake_observations(
+        distribution="gaussian", n_treatments=6, n_observations=6 * 50, random_seed=123
+    )
+
+
+def test_hypothesis_test_group(test_data):
+    exp = Experiment(data=test_data)
 
     # run 'A/A' test (should never reject null)
     test_aa = HypothesisTest(
@@ -12,7 +21,7 @@ def test_hypothesis_test_suite(proportions_data_large):
         control="A",
         variation="A",
         hypothesis="larger",
-        inference_method="proportions_delta",
+        inference_method="means_delta",
     )
     # run A/B test
     test_ab = HypothesisTest(
@@ -20,25 +29,31 @@ def test_hypothesis_test_suite(proportions_data_large):
         control="A",
         variation="B",
         hypothesis="larger",
-        inference_method="proportions_delta",
+        inference_method="means_delta",
     )
-    test_suite = HypothesisTestSuite([test_aa, test_ab], correction_method="b")
-    test_suite_results = exp.run_test_suite(test_suite)
+    correction_method = "b"  # shorthand for Bonferonni
+    test_group = HypothesisTestGroup(
+        tests=[test_aa, test_ab], correction_method=correction_method
+    )
+    test_group_results = exp.run_test_group(test_group)
 
-    assert test_suite_results.corrected_results[0].correction_method == "bonferroni"
-    assert test_suite_results.original_results[0].correction_method is None
+    assert (
+        test_group_results.corrected_results[0].correction_method.__name__
+        == "bonferroni_correction"
+    )
+    assert test_group_results.original_results[0].correction_method is None
 
     # corrected alpha should be smaller
-    alpha_orig = test_suite_results.original_results[0].alpha
-    alpha_corrected = test_suite_results.corrected_results[0].alpha
+    alpha_orig = test_group_results.original_results[0].alpha
+    alpha_corrected = test_group_results.corrected_results[0].alpha
 
     assert alpha_orig > alpha_corrected
-    assert not test_suite_results.corrected_results[0].accept_hypothesis
-    assert test_suite_results.corrected_results[1].accept_hypothesis
+    assert not test_group_results.corrected_results[0].accept_hypothesis
+    assert test_group_results.corrected_results[1].accept_hypothesis
 
 
-def test_custom_metric(means_data):
-    exp = Experiment(means_data, name="means-test")
+def test_custom_metric(test_data):
+    exp = Experiment(test_data)
 
     def custom_metric(row):
         return 4 + np.random.rand() if row["treatment"] != "A" else np.random.rand()
@@ -46,12 +61,14 @@ def test_custom_metric(means_data):
     test_ab = HypothesisTest(
         metric=CustomMetric(custom_metric),
         control="A",
-        variation="F",
+        variation="B",
         hypothesis="larger",
         inference_method="means_delta",
     )
     results_ab = exp.run_test(test_ab)
     results_ab.to_dataframe()
 
-    assert results_ab.test_statistic == "z"
+    results_ab.display()
+
+    assert results_ab.test_statistic_name == "z"
     assert results_ab.accept_hypothesis
