5bbe507da47647dd7a482c6980f383227d918a07
Dustin Stansbury <dustinstansbury@gmail.com>
2023-10-30 11:16:35 -0700

    Refactor test_bootstrap_delta

--- test/test_bootstrap_delta.py
+++ test/test_bootstrap_delta.py
@@ -1,92 +1,117 @@
-from abra import Experiment, HypothesisTest
+import pytest
+
 from numpy import median
 
-def test_small_default_bootstrap_unequal_ab_test(proportions_data_large):
-    exp = Experiment(proportions_data_large, name='proportions-test')
+from spearmint import Experiment, HypothesisTest
+from spearmint.utils import generate_fake_observations
+
+
+@pytest.fixture()
+def proportions_data():
+    return generate_fake_observations(
+        distribution="bernoulli",
+        n_treatments=6,
+        n_attributes=1,
+        n_observations=6 * 1000,
+        random_seed=123,
+    )
+
+
+def test_small_default_bootstrap_unequal_ab_test(proportions_data):
+    exp = Experiment(proportions_data)
 
     # run A/B test
     test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='B',
-        hypothesis='unequal',
-        inference_method='bootstrap'
+        metric="metric",
+        control="A",
+        variation="B",
+        hypothesis="unequal",
+        inference_method="bootstrap",
     )
     results_ab = exp.run_test(test_ab)
 
-    assert results_ab.test_statistic == 'mean'
+    assert results_ab.test_statistic_name == "bootstrap_mean"
     assert results_ab.accept_hypothesis
 
 
-def test_small_default_bootstrap_unequal_aa_test(proportions_data_small):
-    exp = Experiment(proportions_data_small, name='proportions-test')
+def test_small_default_bootstrap_unequal_aa_test(proportions_data):
+    exp = Experiment(proportions_data)
 
     # run A/B test
     test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='A',
-        hypothesis='unequal',
-        inference_method='bootstrap'
+        metric="metric",
+        control="A",
+        variation="A",
+        hypothesis="unequal",
+        inference_method="bootstrap",
     )
+
     results_ab = exp.run_test(test_ab)
 
-    assert results_ab.test_statistic == 'mean'
+    assert results_ab.test_statistic_name == "bootstrap_mean"
     assert not results_ab.accept_hypothesis
 
 
-def test_small_default_bootstrap_smaller_ab_test(proportions_data_small):
-    exp = Experiment(proportions_data_small, name='proportions-test')
+def test_small_default_bootstrap_smaller_ab_test(proportions_data):
+    exp = Experiment(proportions_data)
 
     # run A/B test
     test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='D',
-        hypothesis='smaller',
-        inference_method='bootstrap'
-    )
-    results_ab = exp.run_test(test_ab)
-
-    assert not results_ab.accept_hypothesis
-
-def test_small_bootstrap_larger_ab_test(proportions_data_small):
-    exp = Experiment(proportions_data_small, name='proportions-test')
-
-    # run A/B test
-    test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='D',
-        hypothesis='smaller',
-        inference_method='bootstrap'
+        metric="metric",
+        control="A",
+        variation="D",
+        hypothesis="smaller",
+        inference_method="bootstrap",
     )
     results_ab = exp.run_test(test_ab)
 
     assert not results_ab.accept_hypothesis
 
-def test_small_median_bootstrap_ab_test(proportions_data_small):
-    exp = Experiment(proportions_data_small, name='proportions-test')
+
+def test_small_bootstrap_larger_ab_test(proportions_data):
+    exp = Experiment(proportions_data)
 
     # run A/B test
     test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='D',
-        hypothesis='larger',
-        inference_method='bootstrap',
+        metric="metric",
+        control="A",
+        variation="D",
+        hypothesis="smaller",
+        inference_method="bootstrap",
+    )
+    results_ab = exp.run_test(test_ab)
+
+    assert not results_ab.accept_hypothesis
+
+
+def test_small_median_bootstrap_ab_test(proportions_data):
+    exp = Experiment(proportions_data)
+
+    # run A/B test
+    test_ab = HypothesisTest(
+        metric="metric",
+        control="A",
+        variation="D",
+        hypothesis="larger",
+        inference_method="bootstrap",
         statistic_function=median,
     )
     results_ab = exp.run_test(test_ab)
 
-    assert results_ab.test_statistic == 'median'
+    assert results_ab.test_statistic_name == "bootstrap_median"
     assert results_ab.accept_hypothesis
 
 
-def test_small_median_bootstrap_smaller_ab_test(proportions_data_small):
-    exp = Experiment(proportions_data_small, name='proportions-test')
+def test_small_median_bootstrap_smaller_ab_test(proportions_data):
+    exp = Experiment(proportions_data)
 
     # run A/B test
     test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='D',
-        hypothesis='smaller',
-        inference_method='bootstrap',
+        metric="metric",
+        control="A",
+        variation="D",
+        hypothesis="smaller",
+        inference_method="bootstrap",
         statistic_function=median,
     )
     results_ab = exp.run_test(test_ab)
@@ -94,18 +119,21 @@ def test_small_median_bootstrap_smaller_ab_test(proportions_data_small):
     assert not results_ab.accept_hypothesis
 
 
-def test_small_median_bootstrap_aa_test(proportions_data_small):
-    exp = Experiment(proportions_data_small, name='proportions-test')
+@pytest.mark.filterwarnings("ignore")
+def test_small_median_bootstrap_aa_test(proportions_data):
+    exp = Experiment(proportions_data)
 
     # run A/B test
     test_ab = HypothesisTest(
-        metric='metric',
-        control='A', variation='A',
-        hypothesis='unequal',
-        inference_method='bootstrap',
+        metric="metric",
+        control="A",
+        variation="A",
+        hypothesis="unequal",
+        inference_method="bootstrap",
         statistic_function=median,
     )
+    # AA test results in division by zero
     results_ab = exp.run_test(test_ab)
 
-    assert results_ab.test_statistic == 'median'
+    assert results_ab.test_statistic_name == "bootstrap_median"
     assert not results_ab.accept_hypothesis
