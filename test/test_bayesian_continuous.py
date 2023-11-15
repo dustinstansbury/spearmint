aed5c004864f0a57389de83a0d898062907e55dd
Dustin Stansbury <dustinstansbury@gmail.com>
2023-11-15 15:39:38 -0800

    Add analytic gaussian tests

--- test/test_bayesian_continuous.py
+++ test/test_bayesian_continuous.py
@@ -6,7 +6,7 @@ from spearmint.utils import generate_fake_observations
 @pytest.fixture()
 def continuous_data():
     return generate_fake_observations(
-        distribution="gaussian", n_treatments=2, n_observations=2 * 100
+        distribution="gaussian", n_treatments=2, n_observations=2 * 100, random_seed=123
     )
 
 
@@ -24,6 +24,7 @@ def test_bayesian_gaussian_ab_mcmc(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert test_results.accept_hypothesis
     assert pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
 
 
@@ -40,6 +41,7 @@ def test_bayesian_gaussian_aa_mcmc(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert not test_results.accept_hypothesis
     assert (
         not pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
     )
@@ -58,6 +60,7 @@ def test_bayesian_gaussian_ab_advi(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert test_results.accept_hypothesis
     assert pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
 
 
@@ -74,6 +77,43 @@ def test_bayesian_gaussian_aa_advi(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert not test_results.accept_hypothesis
+    assert (
+        not pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
+    )
+
+
+def test_bayesian_gaussian_ab_analytic(continuous_data):
+    exp = Experiment(data=continuous_data)
+    test = HypothesisTest(
+        inference_method="gaussian",
+        metric="metric",
+        control="A",
+        variation="B",
+        parameter_estimation_method="analytic",  # use Analytic parameter estimation
+    )
+
+    test_results = exp.run_test(test)
+    test_results.display()
+
+    assert test_results.accept_hypothesis
+    assert pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
+
+
+def test_bayesian_gaussian_aa_analytic(continuous_data):
+    exp = Experiment(data=continuous_data)
+    test = HypothesisTest(
+        inference_method="gaussian",
+        metric="metric",
+        control="A",
+        variation="A",
+        parameter_estimation_method="analytic",  # use Analytic parameter estimation
+    )
+
+    test_results = exp.run_test(test)
+    test_results.display()
+
+    assert not test_results.accept_hypothesis
     assert (
         not pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
     )
@@ -92,6 +132,7 @@ def test_bayesian_student_t_ab_mcmc(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert test_results.accept_hypothesis
     assert pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
 
 
@@ -107,6 +148,7 @@ def test_bayesian_student_t_aa_mcmc(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert not test_results.accept_hypothesis
     assert (
         not pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
     )
@@ -125,6 +167,7 @@ def test_bayesian_student_t_ab_advi(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert test_results.accept_hypothesis
     assert pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
 
 
@@ -141,6 +184,7 @@ def test_bayesian_student_t_aa_advi(continuous_data):
     test_results = exp.run_test(test)
     test_results.display()
 
+    assert not test_results.accept_hypothesis
     assert (
         not pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
     )
