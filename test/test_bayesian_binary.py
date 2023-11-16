b4f3ae6ab90fa5bde9db4cb816b7d03eb9bf5581
Dustin Stansbury <dustinstansbury@gmail.com>
2023-11-16 09:21:49 -0800

    Add model param update tests for binary models

--- test/test_bayesian_binary.py
+++ test/test_bayesian_binary.py
@@ -13,6 +13,22 @@ def binary_data():
     )
 
 
+def test_bernoulli_mcmc_model_params(binary_data):
+    exp = Experiment(data=binary_data)
+
+    test = HypothesisTest(
+        inference_method="bernoulli",
+        metric="metric",
+        control="A",
+        variation="C",
+        parameter_estimation_method="mcmc",
+        model_params=dict(prior_alpha=2.0),
+    )
+    test_results = exp.run_test(test)
+    assert test_results.model_hyperparams["prior_alpha"] == 2
+    assert test_results.model_hyperparams["prior_beta"] == 1  # default
+
+
 def test_bernoulli_ab_mcmc(binary_data):
     exp = Experiment(data=binary_data)
 
@@ -83,6 +99,22 @@ def test_bernoulli_aa_advi(binary_data):
     )
 
 
+def test_bernoulli_analytic_model_params(binary_data):
+    exp = Experiment(data=binary_data)
+
+    test = HypothesisTest(
+        inference_method="bernoulli",
+        metric="metric",
+        control="A",
+        variation="C",
+        parameter_estimation_method="analytic",
+        model_params=dict(prior_alpha=2.0),
+    )
+    test_results = exp.run_test(test)
+    assert test_results.model_hyperparams["prior_alpha"] == 2
+    assert test_results.model_hyperparams["prior_beta"] == 1  # default
+
+
 def test_bernoulli_aa_analytic(binary_data):
     exp = Experiment(data=binary_data)
     test = HypothesisTest(
@@ -115,6 +147,23 @@ def test_bernoulli_ab_analytic(binary_data):
     test_results.display()
     assert test_results.accept_hypothesis
     assert pytest.approx(test_results.prob_greater_than_zero, rel=0.1, abs=0.01) == 1.0
+
+
+def test_binomial_mcmc_model_params(binary_data):
+    exp = Experiment(data=binary_data)
+
+    test = HypothesisTest(
+        inference_method="binomial",
+        metric="metric",
+        control="A",
+        variation="C",
+        parameter_estimation_method="mcmc",
+        model_params=dict(prior_alpha=2.0, possible_outcomes=3.0),
+    )
+    test_results = exp.run_test(test)
+    assert test_results.model_hyperparams["prior_alpha"] == 2
+    assert test_results.model_hyperparams["possible_outcomes"] == 3
+    assert test_results.model_hyperparams["prior_beta"] == 1  # default
 
 
 def test_binomial_ab_mcmc(binary_data):
@@ -172,6 +221,22 @@ def test_binomial_advi(binary_data):
 
     with pytest.raises(UnsupportedParameterEstimationMethodException):
         exp.run_test(test)
+
+
+def test_binomial_analytic_model_params(binary_data):
+    exp = Experiment(data=binary_data)
+
+    test = HypothesisTest(
+        inference_method="binomial",
+        metric="metric",
+        control="A",
+        variation="C",
+        parameter_estimation_method="analytic",
+        model_params=dict(prior_alpha=2.0),
+    )
+    test_results = exp.run_test(test)
+    assert test_results.model_hyperparams["prior_alpha"] == 2
+    assert test_results.model_hyperparams["prior_beta"] == 1  # default
 
 
 def test_binomial_aa_analytic(binary_data):
